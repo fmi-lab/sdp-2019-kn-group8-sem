a1d6d4ab5a1ca301491a7d69dd75533c582294d1
Antoni <anthvt@gmail.com>
2019-10-24 23:56:18 +0300

    week4

---
+++ week4/stack_based_queue.cpp
@@ -0,0 +1,26 @@
+class StackBasedQueue {
+public:
+    StackBasedQueue() {
+        
+    }
+    
+    /** Push element x to the back of queue. */
+    void push(int x) {
+        
+    }
+    
+    /** Removes the element from in front of queue and returns that element. */
+    int pop() {
+        
+    }
+    
+    /** Get the front element. */
+    int peek() {
+        
+    }
+    
+    /** Returns whether the queue is empty. */
+    bool empty() {
+        
+    }
+};
