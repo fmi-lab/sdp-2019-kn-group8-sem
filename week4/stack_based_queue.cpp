acdc42f22ca5a6fc1eb7b0bbf9459be7574d6875
tozka <anthvt@gmail.com>
2019-11-08 10:09:33 +0200

    Update stack_based_queue.cpp

--- week4/stack_based_queue.cpp
+++ week4/stack_based_queue.cpp
@@ -1,26 +1,34 @@
 class StackBasedQueue {
+private:
+    std::stack<int> pushStack;
+    std::stack<int> popStack;
+
 public:
-    StackBasedQueue() {
-        
-    }
-    
+
     /** Push element x to the back of queue. */
     void push(int x) {
-        
+        pushStack.push(x);
     }
-    
+
     /** Removes the element from in front of queue and returns that element. */
     int pop() {
-        
+        this->peek();
+        popStack.pop();
     }
-    
+
     /** Get the front element. */
     int peek() {
-        
+        if (popStack.empty()) {
+            while (!pushStack.empty()) {
+                popStack.push(pushStack.top());
+                pushStack.pop();
+            }
+        }
+        return popStack.top();
     }
-    
+
     /** Returns whether the queue is empty. */
     bool empty() {
-        
+        return pushStack.empty() && popStack.empty();
     }
-};
+}; 
